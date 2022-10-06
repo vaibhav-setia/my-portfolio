4fbfdb120d76bd67fcecc7492b9111b0245be502
Lakshay Setia <vaibhavwimpsta@gmail.com>
2022-10-07 03:18:05 +0530

    updated photos and resume link

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import data from '../data/data.json';
+import React from "react";
+import data from "../data/data.json";
 
 const Header = (props) => {
-    const headerStyles = {
-        
-    };
-    return(
-        <div className="header" style={{backgroundColor: data.header.backgroundColor, color: data.header.fontPrimaryColor}}>
-            <div className="header-left">
-                <div>{data.firstName}</div>
-                <div>{data.lastName}</div>
-            </div>
+  const headerStyles = {};
+  return (
+    <div
+      className="header"
+      style={{
+        backgroundColor: data.header.backgroundColor,
+        color: data.header.fontPrimaryColor,
+      }}
+    >
+      <div className="header-left">
+        <a href="#">
+          <div>{data.firstName}</div>
+          <div>{data.lastName}</div>
+        </a>
+      </div>
 
-            <div className="header-right">
-                <span><a href="#about">ABOUT ME</a></span>
-                <span><a href="#skills">SKILLS</a></span>
-                <span><a href="#projects">PROJECTS</a></span>
-                <span><a href="#experience">EXPERIENCE</a></span>
-                <span><a href="/downloadable/resume.pdf" target="_blank">RESUME</a></span>
-            </div>
-        </div>
-    )
+      <div className="header-right">
+        <span>
+          <a href="#about">ABOUT ME</a>
+        </span>
+        <span>
+          <a href="#skills">SKILLS</a>
+        </span>
+        <span>
+          <a href="#projects">PROJECTS</a>
+        </span>
+        <span>
+          <a href="#experience">EXPERIENCE</a>
+        </span>
+        <span>
+          <a
+            href="https://drive.google.com/file/d/1YvwXUzN0Tx-3IWMsCtdZHXY2Fknc12cw/preview"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RESUME
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
