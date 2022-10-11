a0344fa88cb7cd421fc7703e3dfba1567f17156b
Lakshay Setia <vaibhavwimpsta@gmail.com>
2022-10-12 00:22:30 +0530

    updated resume

--- src/js/Header.js
+++ src/js/Header.js
@@ -33,7 +33,7 @@ const Header = (props) => {
         </span>
         <span>
           <a
-            href="https://drive.google.com/file/d/1tidsUUCYB57PtTXfn6jP06hAlOMn0igT/preview"
+            href="https://drive.google.com/file/d/1QzAdCytqXmIN-T2mGEVNsm3Y7MvfVvaO/preview"
             target="_blank"
             rel="noopener noreferrer"
           >
